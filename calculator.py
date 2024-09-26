0c87084a7e3263b40c620622871bc8a63ce47886
Simon Kuang <skuang64@gmail.com>
2024-09-26 17:13:38 -0400

    commited

---
+++ calculator.py
@@ -0,0 +1,13 @@
+import sympy as sp
+
+# Define the matrix
+A = sp.Matrix([[1, -1, 1, 1, 6], [2, 0, -1, -1, 5], [2, -2, 0, 1, 4], [0, 1, 1, -1, 3]])
+
+# Get the row echelon form and pivot columns
+rref_matrix, pivot_columns = A.rref()
+
+# Print the Row Echelon Form
+print("Row Echelon Form:")
+print(rref_matrix)
+
+# Print the pivot columns (the columns that contain pivots)
